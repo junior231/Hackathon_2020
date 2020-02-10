a24d7892674d9185ea810bc0d86813e5acb390e1
Ilo <c_ilo@ats-hr420-dm32.local>
2020-02-10 11:05:07 -0500

    created components for vue

---
+++ js/components/promotionComponent.js
@@ -0,0 +1,11 @@
+export default {
+    template: `
+        <div class="container"
+            <div class="row">
+                <div class="">
+                    <h1 class=""> {{ message }} </h1>
+                </div>
+            </div>
+        </div>
+    `,
+}
